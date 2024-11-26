8e99ecd62ac187370f38056b7b82b3ac45bb3a6e
Tamara Plante <0631064@johnabbottcollege.net>
2024-11-26 13:35:23 -0500

    Update datagrid with proper query
    Still needs work.

--- ViewModels/TimeEntriesViewModel.cs
+++ ViewModels/TimeEntriesViewModel.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Entity;
+using System.Data.Linq.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using ticksy.Helpers;
 
 namespace ticksy.ViewModels
 {
     public class TimeEntriesViewModel : AViewModel
     {
-        public List<TimeEntry> TimeEntries { get; set; }
-        private User User {  get; set; }
+        public ObservableCollection<TimeEntrySummary> TimeEntries { get; set; }
+        private User User { get; set; }
 
         public TimeEntriesViewModel(User user)
         {
@@ -19,13 +23,29 @@ namespace ticksy.ViewModels
 
             try
             {
-                // We have to change database table structure and add UserId...
-                TimeEntries = Globals.DbContext.Set<TimeEntry>().ToList();
+                var entries = Globals.DbContext.Set<TimeEntry>()
+                    .Where(t => t.UserId == User.UserId)
+                    .Select(t => new TimeEntrySummary
+                    {
+                        Name = t.Name, Task = t.Task.Name, StartTime = t.StartTime, EndTime = t.EndTime
+                    })
+                    .ToList();
+
+                TimeEntries = new ObservableCollection<TimeEntrySummary>(entries);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
+    }
+    
+    public class TimeEntrySummary
+    {
+        public string Name { get; set; }
+        public string Task { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Elapsed { get => EndTime - StartTime; }
     }
 }
