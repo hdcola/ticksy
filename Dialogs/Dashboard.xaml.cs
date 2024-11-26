e299b76e9588f31eeaf159c23c8c9c1fd3559cae
Tamara Plante <0631064@johnabbottcollege.net>
2024-11-26 07:27:02 -0500

    Switch to MVVM time entries

--- Dialogs/Dashboard.xaml.cs
+++ Dialogs/Dashboard.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ticksy.Views;
 
 namespace ticksy.Dialogs
 {
@@ -22,6 +25,11 @@ namespace ticksy.Dialogs
         public Dashboard()
         {
             InitializeComponent();
+        }
+        private void TimeEntriesStackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            User user = Globals.User;
+            TabTimeEntries.Content = new TimeEntriesView(user, this);
         }
     }
 }
---
+++ ViewModels/TimeEntriesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ticksy.Helpers;
+
+namespace ticksy.ViewModels
+{
+    public class TimeEntriesViewModel : AViewModel
+    {
+        public List<TimeEntry> TimeEntries { get; set; }
+        private User User {  get; set; }
+
+        public TimeEntriesViewModel(User user)
+        {
+            User = user;
+
+            try
+            {
+                // We have to change database table structure and add UserId...
+                TimeEntries = Globals.DbContext.Set<TimeEntry>().ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
