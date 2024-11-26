77c69838d0c2fafa99f279bfc48b6354831cfdd5
OldFangDong <gh@hdcola.org>
2024-11-25 20:46:46 -0500

    support delete a item

--- Helpers/Invoice.cs
+++ Helpers/Invoice.cs
@@ -4,11 +4,16 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ticksy.Helpers
 {
     public class Invoice
     {
+        public Invoice()
+        {
+            DeleteCommand = new RelayCommand<InvoiceItem>(DeleteItem);
+        }
         public string InvoiceNumber { get; set; }
         public DateTime InvoiceDate { get; set; }
         public DateTime DueDate { get; set; }
@@ -16,6 +21,18 @@ namespace ticksy.Helpers
         public string BillFrom { get; set; }
 
         public ObservableCollection<InvoiceItem> Items { get; set; }
+
+        public ICommand DeleteCommand { get; }
+
+        private void DeleteItem(InvoiceItem item)
+        {
+            if (item != null && Items.Contains(item))
+            {
+                Items.Remove(item);
+            }
+
+        }
+
 
         public override string ToString()
         {
@@ -32,6 +49,37 @@ namespace ticksy.Helpers
         public override string ToString()
         {
             return $"Description: {Description}, Quantity: {Quantity}, Price: {Price}";
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is T validParameter)
+            {
+                _execute(validParameter);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
     }
 }
